6b723bb311beb27f4245e311fd6ff0ee43bb8e40
Vivek YV <yvvivek9@gmail.com>
2025-02-12 07:36:09 +0530

    12th feb

--- server/static/flutter_bootstrap.js
+++ server/static/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1900637417"
+    serviceWorkerVersion: "2680247949"
   }
 });
